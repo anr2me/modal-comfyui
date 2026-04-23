e183964c88cf9ac857fc9eb8afac097b00b16a9e
AdamN <7974720+anr2me@users.noreply.github.com>
2026-04-23 10:07:35 +0700

    Add external plugin downloads template
    Added a section for external plugin downloads with a template.

--- plugins.example.py
+++ plugins.example.py
@@ -2,3 +2,12 @@ comfy_plugins = [
     # put comfyui custom node id here
     # IMPORTANT: node id from comfyui registry (Not node name)
 ]
+
+comfy_plugins_ext = [
+    # External downloads (via git).
+    # {
+    #     "url": "URL",
+    #     "branch": "BRANCH",
+    #     "install": "requirements.txt", # or "install.py"
+    # },
+]
