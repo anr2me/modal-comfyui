61e62e69d39eb7684069018f76495ad03a860a8a
AdamN <7974720+anr2me@users.noreply.github.com>
2026-04-23 14:29:49 +0700

    Test 8

--- comfyui.py
+++ comfyui.py
@@ -23,7 +23,7 @@ def get_comfyui_path() -> Path:
         # Example output: "Current workspace: /home/user/comfy/ComfyUI"
         if ":" in result:
             comfyui_path = Path(result.split(":", 1)[1].strip())
-            print(f"Extracted Path: {comfyui_path}")
+            print(f"ComfyUI Path: {comfyui_path}")
         else:
             print("Path not found in output")
     except FileNotFoundError:
@@ -146,6 +146,7 @@ image = (
     .apt_install("git", "git-lfs", "libgl1-mesa-dev", "libglib2.0-0", "aria2")
     .uv_pip_install("pip", "uv")
     .pip_install_from_requirements(str(root_dir / "requirements_comfy.txt")) # uv=True
+    .run_commands("mkdir -p /cache/ComfyUI")
     .run_commands("comfy --skip-prompt --no-enable-telemetry tracking disable")
     .run_commands("comfy --skip-prompt set-default /cache/ComfyUI", volumes={"/cache": vol})
     .run_commands("comfy --skip-prompt install --nvidia --cuda-version 13.0", volumes={"/cache": vol})
