384dc48dd4dae86f12be236984bf1faeec079184
AdamN <7974720+anr2me@users.noreply.github.com>
2026-04-23 14:21:41 +0700

    oops

--- comfyui.py
+++ comfyui.py
@@ -146,7 +146,7 @@ image = (
     .apt_install("git", "git-lfs", "libgl1-mesa-dev", "libglib2.0-0", "aria2")
     .uv_pip_install("pip", "uv")
     .pip_install_from_requirements(str(root_dir / "requirements_comfy.txt")) # uv=True
-    .run_commands("comfy tracking disable")
+    .run_commands("comfy --skip-prompt --no-enable-telemetry tracking disable")
     .run_commands("comfy --skip-prompt set-default /cache/ComfyUI", volumes={"/cache": vol})
     .run_commands("comfy --skip-prompt install --nvidia --cuda-version 13.0", volumes={"/cache": vol})
     .run_commands("git lfs install")
