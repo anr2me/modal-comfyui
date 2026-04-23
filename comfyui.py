0c77976159b56b96b4e7e575486e2abaad39429b
AdamN <7974720+anr2me@users.noreply.github.com>
2026-04-23 13:42:07 +0700

    Test 5

--- comfyui.py
+++ comfyui.py
@@ -146,7 +146,8 @@ image = (
     .apt_install("git", "git-lfs", "libgl1-mesa-dev", "libglib2.0-0", "aria2")
     .uv_pip_install("pip", "uv")
     .pip_install_from_requirements(str(root_dir / "requirements_comfy.txt"), uv=True)
-    .run_commands("comfy --skip-prompt install --nvidia --cuda-version 13.0")
+    .run_commands("comfy set-default /cache/ComfyUI", volumes={"/cache": vol})
+    .run_commands("comfy --skip-prompt install --nvidia --cuda-version 13.0", volumes={"/cache": vol})
     .run_commands("git lfs install")
 )
 
@@ -159,7 +160,7 @@ image = image.add_local_file(
         "/root/comfy/ComfyUI/extra_model_paths.yaml", 
         copy=True
 )
-.run_commands("comfy set-default /cache/ComfyUI", volumes={"/cache": vol})
+#.run_commands("comfy set-default /cache/ComfyUI", volumes={"/cache": vol})
 #.run_commands("comfy --skip-prompt install --nvidia --cuda-version 13.0", volumes={"/cache": vol})
 #.run_commands("git lfs install")
 
