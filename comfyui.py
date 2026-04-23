5b1c8984633cf18fb3badedce6bb305cd2cf8b6b
AdamN <7974720+anr2me@users.noreply.github.com>
2026-04-24 03:35:15 +0700

    oops

--- comfyui.py
+++ comfyui.py
@@ -135,9 +135,11 @@ def download_external_plugin(url: str, branch: str, install: str):
 
 def download_all():
     global image
-    image = image.run_commands("comfy --skip-prompt --workspace /cache/ComfyUI install --nvidia --cuda-version 13.0 || true", volumes={"/cache": vol})
-    .run_commands("comfy --skip-prompt set-default /cache/ComfyUI", volumes={"/cache": vol})
-    .run_commands("git lfs install") # --skip-smudge
+    image = (
+        image.run_commands("comfy --skip-prompt --workspace /cache/ComfyUI install --nvidia --cuda-version 13.0 || true", volumes={"/cache": vol})
+        .run_commands("comfy --skip-prompt set-default /cache/ComfyUI", volumes={"/cache": vol})
+        .run_commands("git lfs install") # --skip-smudge
+    )
 
     for model in models:
         hf_download(model["repo_id"], model["filename"], model["model_dir"])
