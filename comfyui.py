39d084d8441838953735b12fc77256d121e126ff
AdamN <7974720+anr2me@users.noreply.github.com>
2026-04-25 09:19:15 +0700

    oops 2

--- comfyui.py
+++ comfyui.py
@@ -214,7 +214,7 @@ secrets=[modal.Secret.from_name("huggingface-secret")]
 image = image.env({
     "HF_HUB_ENABLE_HF_TRANSFER": "1", 
     "HF_TOKEN": os.environ.get("HF_TOKEN"),
-}).run_function(
+}, secrets=[modal.Secret.from_name("huggingface-secret")]).run_function(
     download_all, volumes={"/cache": vol}
 ).run_function(
     install_missing_deps, volumes={"/cache": vol}
