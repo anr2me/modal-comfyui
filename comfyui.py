dfbfdd45d9a269ac433e8b3aea6941dee71c45af
AdamN <7974720+anr2me@users.noreply.github.com>
2026-04-26 08:57:34 +0700

    Testing 04

--- comfyui.py
+++ comfyui.py
@@ -187,11 +187,11 @@ def install_missing_deps():
     
     global image
     image = image.uv_pip_install("cupy-cuda13x")
-    image = image.uv_pip_install("sageattention==2.2.0", extra_options="--no-build-isolation --extra-index-url https://comfy-org.github.io/wheels") #sageattn3 
+    image = image.pip_install("sageattention==2.2.0", extra_options="--no-build-isolation --extra-index-url https://comfy-org.github.io/wheels") #sageattn3 
     image = image.uv_pip_install("flash-attn-3", extra_options="--no-build-isolation --extra-index-url https://download.pytorch.org/whl/cu130") #flash-attn-4[cu13]
     image = image.uv_pip_install(f"https://github.com/nunchaku-tech/nunchaku/releases/download/v1.2.1/nunchaku-1.2.1+cu13.0torch{pytorch_version_number}-cp313-cp313-linux_x86_64.whl")
     
-    image.run_commands("uv pip show cupy-cuda13x sageattention flash-attn-3 nunchaku")
+    image = image.run_commands("uv pip show cupy-cuda13x sageattention flash-attn-3 nunchaku; exit 1")
     print("Done install missing dependencies.")
 
 
