b531153a506421cf8f516dad8ae5dd100c8e18e1
AdamN <7974720+anr2me@users.noreply.github.com>
2026-04-26 14:16:06 +0700

    Testing 08

--- comfyui.py
+++ comfyui.py
@@ -186,9 +186,9 @@ def install_missing_deps():
     pytorch_version_number = ".".join(full_pytorch_version.split(" ")[0].split(".")[:2])
     print(f"PyTorch Ver = {pytorch_version_number}")
     
-    global image
+    #global image
     print(f"Testing2 Global Base Dir: {base_dir}, Image: {image}")
-    image = image.uv_pip_install("cupy-cuda13x")
+    image = image.uv_pip_install("cupy-cuda13x this_should_fail")
     image = image.run_commands("pip install sageattention==2.* --no-build-isolation --extra-index-url https://comfy-org.github.io/wheels; exit 1")
     image = image.pip_install("sageattention==2.2.0", extra_options="--no-build-isolation --extra-index-url https://comfy-org.github.io/wheels") #sageattn3 
     image = image.uv_pip_install("flash-attn-3", extra_options="--no-build-isolation --extra-index-url https://download.pytorch.org/whl/cu130") #flash-attn-4[cu13]
