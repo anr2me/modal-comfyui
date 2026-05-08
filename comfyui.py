efa2e47000d07c906918f6c7a59e68e436ec5339
AdamN <7974720+anr2me@users.noreply.github.com>
2026-05-08 15:42:59 +0700

    oops 3

--- comfyui.py
+++ comfyui.py
@@ -339,7 +339,7 @@ gpuport = uiport + 1
 
 def get_remote_url(class_name: str) -> str:
     remote_cls = modal.Cls.from_name(app.name, class_name)
-    url = remote_cls().web.get_web_url()
+    url = await remote_cls().web.get_web_url.aio()
     return url
     
 @web_app.get("/prompt")
