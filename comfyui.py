db35cc2aed9dead28d17adfa77ed06320fa0cd05
AdamN <7974720+anr2me@users.noreply.github.com>
2026-05-19 17:40:43 +0700

    Try internal/logs on GPU

--- comfyui.py
+++ comfyui.py
@@ -453,7 +453,7 @@ async def proxy_interrupt(request: Request):
  
     return new_resp
 
-@web_app.get("/api/view")
+#@web_app.get("/api/view")
 @web_app.get("/api/jobs")
 async def proxy_jobs(request: Request):
     #url = f"http://127.0.0.1:{uiport}"
@@ -465,10 +465,11 @@ async def proxy_jobs(request: Request):
     return new_resp
 
 # Proxy other API routes
-@web_app.get("/api/{path:path}")
+@web_app.get("/internal/logs{path:path}")
+#@web_app.get("/api/{path:path}")
 async def proxy_api(request: Request, path: str):
-    url = f"http://127.0.0.1:{uiport}"
-    #url = await get_remote_url("ComfyGPU")
+    #url = f"http://127.0.0.1:{uiport}"
+    url = await get_remote_url("ComfyGPU")
 
     # Forward request
     new_resp = await forward_httpx(url, request)
