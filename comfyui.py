1f701e51f2818d778f435675d108a7ddccb95399
AdamN <7974720+anr2me@users.noreply.github.com>
2026-05-19 17:30:50 +0700

    Try view API on GPU

--- comfyui.py
+++ comfyui.py
@@ -453,6 +453,7 @@ async def proxy_interrupt(request: Request):
  
     return new_resp
 
+@web_app.get("/api/view")
 @web_app.get("/api/jobs")
 async def proxy_jobs(request: Request):
     #url = f"http://127.0.0.1:{uiport}"
@@ -466,8 +467,8 @@ async def proxy_jobs(request: Request):
 # Proxy other API routes
 @web_app.get("/api/{path:path}")
 async def proxy_api(request: Request, path: str):
-    #url = f"http://127.0.0.1:{uiport}"
-    url = await get_remote_url("ComfyGPU")
+    url = f"http://127.0.0.1:{uiport}"
+    #url = await get_remote_url("ComfyGPU")
 
     # Forward request
     new_resp = await forward_httpx(url, request)
