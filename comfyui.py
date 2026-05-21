d3f4a73d2b8ab5a3c03816bbc38623d0fb22d1db
AdamN <7974720+anr2me@users.noreply.github.com>
2026-05-21 19:55:51 +0700

    oops  --no-build-isolation will cause Building nvidia-vfx to failed

--- comfyui.py
+++ comfyui.py
@@ -261,7 +261,7 @@ if comfy_plugins_ext:
         plugin_reqs = plugin.get("requirements", "").strip()
         if plugin_reqs:
             formatted_reqs = " ".join(f"-r {file}" for file in plugin_reqs.split())
-            image = image.run_commands(f"cd {nodes_dir}/{folder_name} && uv pip install --no-deps --no-build-isolation --python $(command -v python) --compile-bytecode {formatted_reqs}")
+            image = image.run_commands(f"cd {nodes_dir}/{folder_name} && uv pip install --no-deps --python $(command -v python) --compile-bytecode {formatted_reqs}")
 
         # run installation script (usually install.py or setup.py)
         plugin_install = plugin.get("install", "").strip()
@@ -274,7 +274,7 @@ if comfy_plugins_ext:
         # install optional packages or packages that got dependency issue with other custom nodes due to pinned to an incompatible version
         plugin_deps = plugin.get("dependencies", "").strip()
         if plugin_deps:
-            image = image.uv_pip_install(plugin_deps.split(), extra_options="--no-deps --no-build-isolation") #, gpu=GPU_MODEL
+            image = image.uv_pip_install(plugin_deps.split(), extra_options="--no-deps") #, gpu=GPU_MODEL
 
 # install missing dependencies or override with a compatible version
 def install_wheels():
