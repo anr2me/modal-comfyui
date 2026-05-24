92b503dd606fab3c204293d4eff70869c891bef8
AdamN <7974720+anr2me@users.noreply.github.com>
2026-05-25 06:10:46 +0700

    removed the flooding logs

--- comfyui.py
+++ comfyui.py
@@ -609,8 +609,8 @@ async def proxy_websocket(websocket: WebSocket):
                                     print(f"Internal websocket is Ready!({comfy_ws.request.headers.get("Host", "")})")
                                     # TODO: Investigate why "pending_prompt" switched from 1 to 0 after this message shows up in the logs (before "Waiting for GPU" message)
                                     print(f"The message = >>> {message} <<<")
-                                pending_prompt = await shared_dict.get.aio("pending_prompt", 0)
-                                print(f"Checked pending_prompt: {pending_prompt}")
+                                #pending_prompt = await shared_dict.get.aio("pending_prompt", 0)
+                                #print(f"Checked pending_prompt: {pending_prompt}")
                                 # Disconnect from GPU instance when there are no running inference anymore
                                 if status_updated:
                                     active_count = await shared_dict.get.aio("active", 0)
