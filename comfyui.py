c97f846a11ac83dbebee0776c6edcd8b932181d8
AdamN <7974720+anr2me@users.noreply.github.com>
2026-05-24 07:19:29 +0700

    only shows 3 lines of high-severity issue

--- comfyui.py
+++ comfyui.py
@@ -310,9 +310,9 @@ nodes_dir = str(get_comfyui_path() / "custom_nodes")
 image = image.run_commands(
     f"python -m venv /tmp/temp_venv && "
     f"/tmp/temp_venv/bin/pip install bandit[toml] && "
-    f"/tmp/temp_venv/bin/bandit -r {nodes_dir} && "
+    f"/tmp/temp_venv/bin/bandit -r {nodes_dir} -n 3 --severity-level=high && " # only shows 3 lines of high-severity issue
     f"rm -rf /tmp/temp_venv" # Cleanup ensures venv is not in the final layer
-    "&& exit 0" # Making sure the image building doesn't failed here
+    " && exit 0" # Making sure the image building doesn't failed here
 ,volumes={"/cache": vol})
 
 # copy custom nodes to base_dir
