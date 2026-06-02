b3033821f5b02c3e94847e2e63185179e29abe45
AdamN <7974720+anr2me@users.noreply.github.com>
2026-06-02 10:37:45 +0700

    doesn't need sudo

--- comfyui.py
+++ comfyui.py
@@ -311,7 +311,7 @@ image = image.run_commands("yolo settings sync=False")
 image = image.run_commands(
     "git clone https://github.com/lining79355504/fork_greenboost.git",
     "cd fork_greenboost",
-    "sudo ./greenboost_setup.sh",
+    "./greenboost_setup.sh",
 )
 
 # Testing for vulnerability on custom nodes
