23d18f34fd4b49c5a0b6158c887080b287574cd2
AdamN <7974720+anr2me@users.noreply.github.com>
2026-06-02 10:42:30 +0700

    set executable mode

--- comfyui.py
+++ comfyui.py
@@ -311,6 +311,7 @@ image = image.run_commands("yolo settings sync=False")
 image = image.run_commands(
     "git clone https://github.com/lining79355504/fork_greenboost.git",
     "cd fork_greenboost",
+    "chmod +x greenboost_setup.sh",
     "./greenboost_setup.sh",
 )
 
