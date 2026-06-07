d5e0e0abb34a00c7559fa2c884e06c6081f545e3
AdamN <7974720+anr2me@users.noreply.github.com>
2026-06-07 09:26:48 +0700

    oops 3

--- comfyui.py
+++ comfyui.py
@@ -348,13 +348,13 @@ from fastapi import FastAPI
 web_app = FastAPI()
 # Enable automatic compression
 web_app.add_middleware(
-    minimum_size=1000,  # Bytes: skip small payloads to protect CPU overhead
     #GZipMiddleware, 
-    #compresslevel=5     # Balance between speed (1) and size reduction (9)
+    #compresslevel=5,     # Balance between speed (1) and size reduction (9)
     CompressMiddleware, # All-in-One compression middleware (Zstd, Brotli, and Gzip)
     zstd_level=10,        # Standard Zstd compression level (1-19)
     brotli_quality=6,   # Brotli: 0 to 11
-    gzip_level=5        # Gzip: 1 to 9
+    gzip_level=5,        # Gzip: 1 to 9
+    minimum_size=1000,  # Bytes: skip small payloads to protect CPU overhead
 )
 
 app = modal.App(name="modal-comfyui", image=image)
