143e2e283df3c108b8aa0feee5f988ccb2453608
AdamN <7974720+anr2me@users.noreply.github.com>
2026-07-21 19:58:11 +0700

    Don't quote the Accept header, otherwise the quote will be included in the header.

--- comfyui.py
+++ comfyui.py
@@ -75,7 +75,7 @@ def download_external_model(url: str, filename: str, model_dir: str):
                     "aria2c",
                     "--console-log-level=info",
                     "--summary-interval=0",
-                    '--header="Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"',
+                    "--header=Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
                     "-x",
                     "16",
                     "-s",
